6a4bf79bbd72471320e3256bf9eac437b2e6baef
kostianych <kostianych@gmail.com>
2011-09-14 01:03:58 +0300

    selenium-webdriver is used

--- test/selenium/add_activity.rb
+++ test/selenium/add_activity.rb
@@ -1,30 +1,49 @@
+require "selenium-webdriver"
 require "test/unit"
-require "rubygems"
-gem "selenium-client"
-require "selenium/client"
 
-class add_activity < Test::Unit::TestCase
+class AddActivity2 < Test::Unit::TestCase
 
   def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
-    @selenium = Selenium::Client::Driver.new \
-      :host => "localhost",
-      :port => 4444,
-      :browser => "*chrome",
-      :url => "http://change-this-to-the-site-you-are-testing/",
-      :timeout_in_second => 60
-
-    @selenium.start_new_browser_session
   end
   
   def teardown
-    @selenium.close_current_browser_session
+    @driver.quit
     assert_equal [], @verification_errors
   end
   
   def test_add_activity
-    @selenium.open "/activities"
-    @selenium.click "link=New activity"
-    @selenium.wait_for_page_to_load "30000"
+    @driver.get "http://0.0.0.0:3000/activities"
+    @driver.find_element(:link, "New activity").click
+
+    verify_text_not_present("Id")
+
+    element = @driver.find_element(:id, 'activity_priority')
+    element.send_keys "20"
+    element.submit
+  end
+  
+  def element_present?(how, what)
+    @driver.find_element(how, what)
+    true
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    false
+  end
+
+  def verify_text_not_present(text)
+    labels = @driver.find_elements(:tag_name, "label")
+    labels.each do |label|
+      assert(label.text != text)    
+    end
+    rescue Test::Unit::AssertionFailedError => ex
+      @verification_errors << ex
+	end
+  
+  def verify(&blk)
+    yield
+  rescue Test::Unit::AssertionFailedError => ex
+    @verification_errors << ex
   end
 end
